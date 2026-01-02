d2013b06f206456896a9747a6fdd45acbd60580a
Omar Arif <oarif@CEN-F-oarif.local>
2026-01-02 08:11:28 +0400

    added

---
+++ finetune_lora_hpc.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#SBATCH --account=acc-oarif
+#SBATCH --partition=gpu
+#SBATCH --nodes=8
+#SBATCH --ntasks=8
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4:00:00
+
+source /opt/miniconda/bin/activate /shared/oarif/envs/lava
+
+export CUDA_HOME=$CONDA_PREFIX
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export TRITON_CACHE_DIR=/scratch/oarif
+
+export WANDB_PROJECT=llava-finetune
+export WANDB_ENTITY=oarif
+export WANDB_RUN_NAME=llava-mistral-lora
+export WANDB_DIR=/shared/oarif/wandb
+
+deepspeed \
+    /shared/oarif/RL4VLM/LLaVA/llava/train/train.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ./zero3_offload.json \
+    --model_name_or_path /shared/oarif/LLaVA/models/llava-v1.6-mistral-7b \
+    --version v1 \
+    --data_path /shared/oarif/datasets/sft-data/alfworld-gpt4-45k.json \
+    --image_folder /shared/oarif/datasets/sft-data/ \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir /shared/oarif/RL4VLM/output \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
